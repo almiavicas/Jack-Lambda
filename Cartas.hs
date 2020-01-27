26d48971a6dd334d9c6ed7c8ab92db10cd1565fe
alvaro <almiavicas@gmail.com>
2020-01-27 18:26:47 -0400

    Separated files as needed

---
+++ Cartas.hs
@@ -0,0 +1,101 @@
+data Palo  = Treboles | Diamantes | Picas | Corazones
+
+data Rango = N Int | Jack | Queen | King | Ace
+
+data Carta = Carta {
+    rango :: Rango,
+    palo  :: Palo
+}
+
+data Jugador = Dealer | Player
+
+newtype Mano = Mano [Carta]
+
+-- Funciones de Construccion
+
+-- Produce una Mano vacia
+vacia :: Mano
+
+-- Produce una Mano que contenga las 52 cartas
+baraja :: Mano
+
+-- Funciones de Acceso
+
+-- Determina la cantidad de cartas en una mano
+cantidad_Cartas :: Mano -> Int
+
+-- Recibe una mano y devuelve un entero con el valor de la misma
+valor :: Mano -> Int
+
+-- Devuelve true si el valor de la mano excede 21, y False de otra forma
+busted :: Mano -> Bool
+
+-- Devuelve true si la mano es un blackjack y False de otra forma
+blackjack :: Mano -> Bool 
+
+-- Recibe la mano del dealer primero, la mano del jugador de segundo y devuelve
+-- el ganador, segun las reglas del juego
+ganador :: Mano -> Mano -> Jugador
+
+-- Recibe una mano y la separa en una tupla (l, c, r) de la siguiente manera:
+-- - Si la mano es de longitud impar, c sera el elemento medio de la lista, y
+--   l, r seran respectivamente las mitades izquierda y derecha restantes.
+-- - Si la mano es de longitud par, l sera la mitad izquierda, c sera el primer
+--   elemento de la mitad derecha, y r sera el resto de la mitad derecha
+separar :: Mano -> (Mano, Carta, Mano)
+
+-- Funciones de Modificacion
+
+-- Esta funcion sera usada para barajar las cartas al inicio de cada ronda.
+-- Para ello, es necesario el tipo de datos StdGen, incluido en el modulo
+-- System.Random. El segundo argumento es la Mano a barajar, y debe devolverse
+-- la mano ya barajada. Para ello, se debe empezar por una Mano vacia para
+-- acumular
+barajar :: StdGen -> Mano -> Mano
+
+-- Recibe la baraja inicial barajada como Mano, y devuelve la Mano inicial de
+-- Lambda tomando las dos primeras cartas, y la baraja resultante de retirar
+-- dichas cartas.
+inicialLambda :: Mano -> (Mano, Mano)
+
+data Mazo = Vacio | Mitad Carta Mazo Mazo
+
+data Eleccion = Izquierdo | Derecho
+
+-- Funciones de Construccion
+
+-- Recibe una Mano y produce un Mazo. El mazo generado debe ser un arbol
+-- binario balanceado por altura. El arbol construido es tal que su recorrido
+-- inorden forme la Mano usada para construirlo. Una de las funciones
+-- propuestas de Mano es util para esto
+desdeMano :: Mano -> Mazo
+
+-- Funciones de Acceso
+
+-- Recibe un mazo y devuelve True si no es vacio y ninguno de sus hijos es
+-- vacio
+puedePicar :: Mazo -> Bool
+
+-- Funciones de Modificacion
+
+-- Recibe un Mazo y produce una Mano tal que si se le aplicara desdeLista,
+-- deberia producir el mismo Mazo
+aplanar :: Mazo -> Mano
+
+-- Recibe el Mazo original y una Mano con las cartas jugadas en la ronda. Debe
+-- eliminar todas las cartas jugadas del Mazo, y luego debe reconstruirlo,
+-- produciendo un Mazo que siga las mismas reglas de construcicion que
+-- desdeMazo. 
+reconstruir :: Mazo -> Mano -> Mazo
+
+-- Recibe el Mazo actual, la mano del jugador y una Eleccion. Debe devolver una
+-- tupla con el Mazo resultante (la eleccion indica que hijo del Mazo se usara)
+-- y la mano resultante. Devolvera Nothing si no quedan cartas que sacar (
+-- aunque esto no deba ocurrir)
+robar :: Mazo -> Mano -> Eleccion -> Maybe (Mazo, Mano)
+
+-- Recibe el mazo actual, lo vuelve a convertir en una Mano en el orden
+-- apropiado, recibe la Mano del dealer, y devuelve la mano resultante de robar
+-- hasta que supere un valor de 16. Devolvera Nothing si no quedan cartas que
+-- sacar (aunque esto no deba ocurrir)
+jugarLambda :: Mazo -> Mano -> Maybe Mano
